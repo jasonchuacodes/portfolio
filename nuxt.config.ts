b6c61af848a7286e2064f254e4453566b42d817e
Jason Clyde Chua <sirj@Jasons-MacBook-Air.local>
2023-07-05 18:13:11 +0800

    setup: install google fonts, nuxt-icons, and setup tailwind config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    srcDir: 'src/',
-    modules: [
-        '@nuxtjs/tailwindcss'
-    ]
-})
+  srcDir: "src/",
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "nuxt-icon",
+    [
+      "@nuxtjs/google-fonts",
+      {
+        families: {
+          Rubik: true,
+          "Russo+One": true,
+        },
+      },
+    ],
+  ],
+});
+
