fc7baf837d83062473e8cafcf4be336a26092526
Jason Clyde Chua <sirj@Jasons-MacBook-Air.local>
2023-12-30 20:34:26 +0800

    [PORT-3] Add animated border on hero banner

--- src/components/template/hero-banner.vue
+++ src/components/template/hero-banner.vue
@@ -1,13 +1,13 @@
 <template>
-  <div
-    class="flex flex-col w-full items-center space-y-2 px-10 py-2 bg-white bg-opacity-5 border-t border-r border-neutral"
-  >
-    <h1 class="font-russo text-h1 text-white uppercase">{{ title }}</h1>
-    <p
-      class="font-rubik text-l1-r tracking-widest font-sans text-primary-500 lowercase"
-    >
-      {{ position }}
-    </p>
+  <div class="hero">
+    <div class="content">
+      <h1 class="font-russo text-h1 text-white uppercase">{{ title }}</h1>
+      <p
+        class="font-rubik text-l1-r tracking-widest font-sans text-primary-500 lowercase"
+      >
+        {{ position }}
+      </p>
+    </div>
   </div>
 </template>
 <script setup lang="ts">
@@ -18,3 +18,47 @@
 
   defineProps<HeroBannerProps>();
 </script>
+<style scoped>
+  .hero {
+    position: relative;
+    padding: 40px 8px;
+    display: flex;
+    width: 100%;
+    justify-content: center;
+    align-items: center;
+    background: transparent;
+    overflow: hidden;
+  }
+  .hero::before {
+    content: "";
+    position: absolute;
+    width: 140%;
+    height: 50%;
+    background: #a0a7ad;
+    animation: animate 4s linear infinite;
+  }
+  .hero::after {
+    content: "";
+    position: absolute;
+    inset: 2px;
+    background: rgb(14, 18, 25);
+  }
+  .content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    position: relative;
+    color: #fff;
+    z-index: 10;
+    justify-content: center;
+  }
+  @keyframes animate {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(350deg);
+    }
+  }
+</style>
+
