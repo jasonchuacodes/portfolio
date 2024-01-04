e439c0a5bc88ff77ed840a46cdb5dd5068e016dc
jasonchuacodes <jasonchuacodes@gmail.com>
2024-01-04 23:31:56 +0800

    [PORT-16] Add line scan in home page

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -15,10 +15,19 @@ const layer0 = computed(() => ({
         ref="target"
         class="relative flex w-full h-full justify-center items-center transition-all ease-out duration-600 overflow-hidden"
     >
+        <div class="scan fixed top-0 left-0 w-full">
+            <div
+                class="scanline-1 absolute left-0 top-0 h-[5px] w-full bg-white/5"
+            ></div>
+            <div
+                class="scanline-2 absolute left-0 top-0 h-[30px] w-full bg-white/5"
+            ></div>
+        </div>
         <div
             :style="layer0"
-            class="absolute w-full h-full bg-dots-pattern bg-repeat bg-cover opacity-[0.08] z-0"
+            class="layer0 absolute w-full h-full bg-dots-pattern bg-repeat bg-cover opacity-[0.08] z-0"
         ></div>
+
         <div
             class="flex flex-col min-w-[560px] max-w-[640px] justify-center items-center space-y-16 text-light z-10"
         >
@@ -38,3 +47,48 @@ const layer0 = computed(() => ({
         </div>
     </div>
 </template>
+<style scoped>
+.scanline-1 {
+    animation: scan 6s linear 1.3s infinite forwards;
+}
+.scanline-2 {
+    animation: scan 8s linear 0s infinite forwards;
+}
+
+@keyframes scan {
+    0% {
+        transform: translateZ(0);
+    }
+    50% {
+        transform: translate3d(0, 100vh, 0);
+    }
+    100% {
+        transform: translate3d(0, 100vh, 0);
+    }
+}
+
+.scan {
+    animation: flicker 0.35s infinite;
+}
+
+@keyframes flicker {
+    0% {
+        opacity: 1;
+    }
+    25% {
+        opacity: 0.8;
+    }
+    35% {
+        opacity: 1;
+    }
+    38% {
+        opacity: 0.1;
+    }
+    40% {
+        opacity: 0.8;
+    }
+    80% {
+        opacity: 1;
+    }
+}
+</style>
