21a1768985aede7b0fa157019e2122bfe29d0076
Jason Clyde Chua <sirj@Jasons-MacBook-Air.local>
2024-01-09 23:35:12 +0800

    [PORT-21] RouteURL should be updated based on current page

--- src/components/base/skill-card.vue
+++ src/components/base/skill-card.vue
@@ -1,52 +1,52 @@
 <script setup lang="ts">
-type SkillCardProps = {
+  type SkillCardProps = {
     title: string;
     skills: string[];
-};
+  };
 
-defineProps<SkillCardProps>();
+  defineProps<SkillCardProps>();
 
-const showSkillInfo = ref(false);
+  const showSkillInfo = ref(false);
 
-const handleHoverOut = () => {
+  const handleHoverOut = () => {
     showSkillInfo.value = false;
-};
-const handleHoverIn = () => {
+  };
+  const handleHoverIn = () => {
     showSkillInfo.value = true;
-};
+  };
 </script>
 <template>
-    <div
-        @mouseenter="handleHoverIn"
-        @mouseleave="handleHoverOut"
-        class="card relative h-40 w-min-24 cursor-pointer"
+  <div
+    @mouseenter="handleHoverIn"
+    @mouseleave="handleHoverOut"
+    class="card relative h-40 w-min-24 cursor-pointer"
+  >
+    <h2
+      class="px-4 py-2 mb-4 font-russo text-center text-h4 tracking-wider capitalize"
+      :class="{ 'text-primary-500': showSkillInfo }"
     >
-        <h2
-            class="px-4 py-2 mb-4 font-russo text-center text-h4 tracking-wider capitalize"
-            :class="{ 'text-primary-500': showSkillInfo }"
+      {{ title }}
+    </h2>
+    <Transition name="skill-info">
+      <div v-if="showSkillInfo">
+        <div
+          v-for="skill in skills"
+          class="flex justify-center text-center w-full p-1 text-white capitalize"
         >
-            {{ title }}
-        </h2>
-        <Transition name="skill-info">
-            <div v-if="showSkillInfo">
-                <div
-                    v-for="skill in skills"
-                    class="flex justify-center text-center w-full p-1 text-white capitalize"
-                >
-                    <ul>
-                        <li>{{ skill }}</li>
-                    </ul>
-                </div>
-            </div>
-        </Transition>
-    </div>
+          <ul>
+            <li>{{ skill }}</li>
+          </ul>
+        </div>
+      </div>
+    </Transition>
+  </div>
 </template>
 <style scoped>
-.card::before {
+  .card::before {
     content: "";
-    border-top: 2px solid rgba(255,255,255,0.5);
-    border-right: 1px solid rgba(255,255,255,0.5);
-    border-left: 1px solid rgba(255,255,255,0.5);
+    border-top: 2px solid rgba(255, 255, 255, 0.5);
+    border-right: 1px solid rgba(255, 255, 255, 0.5);
+    border-left: 1px solid rgba(255, 255, 255, 0.5);
     position: absolute;
     width: 100%;
     height: 4px;
@@ -55,33 +55,34 @@ const handleHoverIn = () => {
     transform: scaleX(1);
     transition: 0.3s;
     transition-timing-function: ease-out;
-}
-.card:hover::before {
+  }
+  .card:hover::before {
     transform: scaleX(1);
-    border-top: 2px solid rgba(255,255,255,0.8);
-}
-.card::after {
+    border-top: 2px solid rgba(255, 255, 255, 0.8);
+  }
+  .card::after {
     content: "";
-    border-bottom: 1px solid rgba(255,255,255,0.5);
-    border-right: 1px solid rgba(255,255,255,0.5);
-    border-left: 1px solid rgba(255,255,255,0.5);
+    border-bottom: 1px solid rgba(255, 255, 255, 0.5);
+    border-right: 1px solid rgba(255, 255, 255, 0.5);
+    border-left: 1px solid rgba(255, 255, 255, 0.5);
     position: absolute;
     width: 100%;
     height: 10px;
     transform: scale(0);
     transition: 0.3s;
     transition-timing-function: ease-out;
-}
-.card:hover::after {
+  }
+  .card:hover::after {
     transform: scale(1);
-}
-.skill-info-enter-active,
-.skill-info-leave-active {
+  }
+  .skill-info-enter-active,
+  .skill-info-leave-active {
     transition: opacity 0.3s ease;
-}
+  }
 
-.skill-info-enter-from,
-.skill-info-leave-to {
+  .skill-info-enter-from,
+  .skill-info-leave-to {
     opacity: 0;
-}
+  }
 </style>
+
--- src/components/sections/work-section.vue
+++ src/components/sections/work-section.vue
@@ -1,67 +1,50 @@
 <script setup lang="ts">
-const projects = [
-    {
-        id: 1,
-        title: "Project 1",
-        filename: "project.png",
-        detail: "my text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when a",
-    },
-    {
-        id: 2,
-        title: "Project 2",
-        filename: "project.png",
-        detail: "unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It ",
-    },
-    {
-        id: 3,
-        title: "Project 3",
-        filename: "project.png",
-        detail: " Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consesum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, fro",
-    },
-    {
-        id: 4,
-        title: "Project 4",
-        filename: "project.png",
-        detail: "on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, Lorem ipsum dolor sit amet.., comes from a line in section ",
-    },
-];
+  import { vElementVisibility } from "@vueuse/components";
+  import projects from "@/data/projects";
 
-const activeId = ref(1);
-const details = ref(projects[0].title);
+  const target = ref(null);
+  const { onElementVisibility } = useElementVisibility("#work");
 
-const setActiveCard = (id: number) => {
+  const activeId = ref(1);
+  const details = ref(projects[0].title);
+
+  const setActiveCard = (id: number) => {
     activeId.value = id;
-};
+  };
 
-const setProjectDetail = (info: string) => {
+  const setProjectDetail = (info: string) => {
     details.value = info;
-};
+  };
 </script>
 <template>
-    <div id="work"
-        class="flex flex-col justify-center items-center h-screen space-y-10 overflow-hidden"
+  <div
+    id="work"
+    class="flex flex-col justify-center items-center h-screen py-10 mb-60 space-y-10 overflow-hidden"
+  >
+    <div
+      ref="target"
+      v-element-visibility="onElementVisibility"
+      class="flex w-full mx-auto justify-center space-x-4"
     >
-        <div class="flex w-full mx-auto justify-center space-x-4">
-            <div v-for="project in projects">
-                <BaseProjectCard
-                    :id="project.id"
-                    :title="project.title"
-                    :filename="project.filename"
-                    :project-detail="project.detail"
-                    :activeId="activeId"
-                    @set-active-card="setActiveCard"
-                    @set-detail="setProjectDetail"
-                />
-            </div>
-        </div>
-        <div
-            class="relative max-w-[640px] h-56 mx-auto overflow-auto text-light"
-        >
-            <label
-                class="flex w-full sticky top-0 text-l1-r uppercase tracking-widest p-2 bg-neutral/50"
-                >Details:
-            </label>
-            <p class="text-p2-r p-2">{{ details }}</p>
-        </div>
+      <div v-for="project in projects">
+        <BaseProjectCard
+          :id="project.id"
+          :title="project.title"
+          :filename="project.filename"
+          :project-detail="project.detail"
+          :activeId="activeId"
+          @set-active-card="setActiveCard"
+          @set-detail="setProjectDetail"
+        />
+      </div>
     </div>
+    <div class="relative max-w-[640px] h-56 mx-auto overflow-auto text-light">
+      <label
+        class="flex w-full sticky top-0 text-l1-r uppercase tracking-widest p-2 bg-neutral/50"
+        >Details:
+      </label>
+      <p class="text-p2-r p-2">{{ details }}</p>
+    </div>
+  </div>
 </template>
+
