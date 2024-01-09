047a2dd8a90af0d7c35b2bbf97e676b752ac8bde
Jason Clyde Chua <sirj@Jasons-MacBook-Air.local>
2024-01-09 23:53:17 +0800

    [PORT-21] Move the sidebarItems array to a different file

---
+++ src/data/sidebar-items.js
@@ -0,0 +1,28 @@
+const sidebarItems = [
+  {
+      id: 1,
+      title: "HOME",
+      linkTo: "#home",
+      icon: "mdi:home",
+  },
+  {
+      id: 2,
+      title: "WORK",
+      linkTo: "#work",
+      icon: "mdi:work",
+  },
+  {
+      id: 3,
+      title: "ABOUT",
+      linkTo: "#about",
+      icon: "mdi:info",
+  },
+  {
+      id: 4,
+      title: "CONTACT",
+      linkTo: "#contact",
+      icon: "mdi:mail",
+  },
+];
+
+export default sidebarItems;
